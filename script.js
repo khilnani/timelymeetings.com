c0b96a717c01a3e9616d390937e3faf1ce24fc3b
Nik Khilnani <nik@khilnani.org>
2021-09-29 15:50:23 +0000

    Cleanup

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-// Based on https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
-// CSS from https://stackoverflow.com/questions/7082257/css-how-to-skin-a-select-box-with-css
-
 let timeInterval = undefined;
 let notificationWarningSent = false
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Notifications_API/Using_the_Notifications_API
+// https://developer.mozilla.org/en-US/docs/Web/API/Notification/Notification
+
+function notifyUser(message) {
+
+  let opts = {
+    requireInteraction: true
+  }
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    console.log('Notifications not supported.');
+    alert(message);
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    console.log('Notifications already granted, attempting to send.');
+    var notification = new Notification(message, opts);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    console.log('Notifications permissions being re-asked.');
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        console.log('Notifications granted after being initially denied, attempting to send.');
+        var notification = new Notification(message, opts);
+      } else {
+        console.log('Notifications denied after ask.');
+        alert(message);
+      }
+    });
+  }
+
+  else {
+    console.log('Notifications denied.');
+    alert(message);
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+}
+
+// Based on https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+// CSS from https://stackoverflow.com/questions/7082257/css-how-to-skin-a-select-box-with-css
 
 function getTimeRemaining(endtime) {
   const total = Date.parse(endtime) - Date.parse(new Date());
@@ -38,7 +83,7 @@ function initializeClock(id, endtime) {
     minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
     secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
 
-    console.log("t.total", t.total);
+    // console.log("t.total", t.total);
 
     if (t.total > 5 * 60 * 1000) {
       document.body.style.backgroundColor = "#00ECB9"; // default
@@ -113,47 +158,3 @@ function setMeetingDuration() {
 
 setMeetingDuration();
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Notifications_API/Using_the_Notifications_API
-// https://developer.mozilla.org/en-US/docs/Web/API/Notification/Notification
-
-function notifyUser(message) {
-
-  let opts = {
-    requireInteraction: true
-  }
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    console.log('Notifications not supported.');
-    alert(message);
-  }
-
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    console.log('Notifications already granted, attempting to send.');
-    var notification = new Notification(message, opts);
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== "denied") {
-    console.log('Notifications permissions being re-asked.');
-    Notification.requestPermission().then(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        console.log('Notifications granted after being initially denied, attempting to send.');
-        var notification = new Notification(message, opts);
-      } else {
-        console.log('Notifications denied after ask.');
-        alert(message);
-      }
-    });
-  }
-
-  else {
-    console.log('Notifications denied.');
-    alert(message);
-  }
-
-  // At last, if the user has denied notifications, and you
-  // want to be respectful there is no need to bother them any more.
-}
