09bf63f9786735892e3b1057b02fab0b07044965
Nik Khilnani <nik@khilnani.org>
2021-09-29 14:54:11 +0000

    Test notifications

--- script.js
+++ script.js
@@ -104,3 +104,29 @@ function setMeetingDuration() {
 }
 
 setMeetingDuration();
+
+function notifyMe(message) {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert(message);
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(message);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(message);
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+}
