f6c600873c4be2678cf6bcbbdf46033a2002b737
khilnani <nik@khilnani.org>
2021-10-02 23:56:59 -0400

    initial html layout port to react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 
-
 import {clearScheduledNotifications, scheduleNotification, sendNotification, sendTestNotification } from './Notifications'
 
-
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -17,26 +14,77 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    await this.sendTestNotification();
+    //await this.sendTestNotification();
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+        <div className="content">
+
+          <h1>Meeting Countdown Timer</h1>
+
+          <p className="meetingTime"><span id="meetingTime"></span></p>
+
+          <div id="clockdiv">
+            <div>
+              <span className="hours"></span>
+              <div className="clocktext">Hours</div>
+            </div>
+            <div>
+              <span className="minutes"></span>
+              <div className="clocktext">Minutes</div>
+            </div>
+            <div>
+              <span className="seconds"></span>
+              <div className="clocktext">Seconds</div>
+            </div>
+          </div>
+
+          <div>
+
+            <p>
+              <label className="label" htmlFor="meetingDuration">Meeting duration:</label>
+              <select className="select" name="meetingDuration" id="meetingDuration" defaultValue="30">
+                <option value="15">15 mins</option>
+                <option value="30">30 mins</option>
+                <option value="45">45 mins</option>
+                <option value="60">1 hour</option>
+                <option value="90">1.5 hours</option>
+                <option value="120">2 hours</option>
+                <option value="150">2.5 hours</option>
+                <option value="180">3 hours</option>
+              </select>
+            </p>
+
+            <p>
+              <label className="label" htmlFor="meetingSpeedy">End 5 minutes early?</label>
+              <input className="checkbox" type="checkbox" id="meetingSpeedy" name="meetingSpeedy"/>
+            </p>
+
+            <p>
+              <label className="label" htmlFor="meetingSlot">Meeting start time: </label>
+              <select className="select" name="meetingSlot" id="meetingSlot" >
+                <option value="-1">Loading ...</option>
+              </select>
+            </p>
+          </div>
+
           <p>
-            <img src={logo} className="App-logo" alt="logo" />
+            <button className="button" onClick={this.sendTestNotification}>
+              Test Notification
+            </button>
+            <button className="button" onClick={this.clearScheduledNotifications}>
+              Clear Notifications
+            </button>
           </p>
+
           <p>
-            <button onClick={this.sendTestNotification}>
-              Send Test Notification
-            </button>
-            <button onClick={this.clearScheduledNotifications}>
-              Clear Scheduled Notifications
-            </button>
+            <span className="tinyText" >
+              <a href="https://github.com/khilnani/timelymeetings.com" target="_blank">Github</a> | <a href="https://khilnani.org" target="_blank">Nik Khilnani</a>
+            </span>
           </p>
-        </header>
-      </div>
+
+        </div>
     );
   }
 }
