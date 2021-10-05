4762994de17d88336ee6911bdb90c6303fb88507
khilnani <nik@khilnani.org>
2021-10-05 15:52:12 -0400

    updated icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'org.khilnani.TimyMeetings',
+  appId: 'org.khilnani.TimelyMeetings',
   appName: 'TimelyMeetings',
   webDir: 'build',
   bundledWebRuntime: false,
