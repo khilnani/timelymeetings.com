e76a2afeea673be1fd6ea1f69d4dded1f4e8cee1
khilnani <nik@khilnani.org>
2021-10-06 15:44:10 -0400

    console.log cleanup

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      if (process.env.NODE_ENV === 'production') {
+        // remove console in production
+        const TerserPlugin = webpackConfig.optimization.minimizer.find((i) => i.constructor.name === 'TerserPlugin');
+        if (TerserPlugin) {
+          TerserPlugin.options.terserOptions.compress['drop_console'] = true;
+        }
+      }
+
+      return webpackConfig;
+    },
+  },
+};
